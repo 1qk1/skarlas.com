3fcba38328e1176d4f3798a29017ab9bfab338dd
1qk1 <1qk231@gmail.com>
2019-10-14 21:32:53 +0300

    add projects carousel

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,9 @@
 import React from "react"
-import ProjectsGrid from "../components/projects/projectsGrid"
+import Carousel from "react-slick"
+import ProjectItem from "../components/projects/projectItem"
 import { graphql } from "gatsby"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 import _ from "lodash"
 
@@ -23,10 +26,23 @@ const projects = ({ data }) => {
     return newPost
   })
 
+  const carouselSettings = {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+
   return (
     <div className="page__content">
-      <div className="gridWrapper">
-        <ProjectsGrid posts={postsWithImages} />
+      <div className="projectsWrapper">
+        <Carousel {...carouselSettings}>
+          {postsWithImages.map((post, index) => (
+            <ProjectItem key={`post-${index}`} post={post} />
+          ))}
+        </Carousel>
       </div>
     </div>
   )
