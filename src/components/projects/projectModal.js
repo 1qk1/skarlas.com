cd7cafed52ada22f0703f1432b98b2d536cd3093
1qk1 <1qk231@gmail.com>
2019-11-18 17:00:40 +0200

    lazy load project modal

--- src/components/projects/projectModal.js
+++ src/components/projects/projectModal.js
@@ -1,12 +1,12 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import Img from "gatsby-image"
 import { IoIosClose, IoIosLink, IoLogoGithub } from "react-icons/io"
-import Modal from "react-modal"
 import Slider from "./slider"
 import Classes from "./projectModal.module.scss"
 import "./projectModal.scss"
 
-Modal.setAppElement("#___gatsby")
+const AsyncModal = lazy(() => import("react-modal"))
+// AsyncModal.setAppElement("#___gatsby")
 
 const customStyles = {
   content: {
@@ -30,81 +30,85 @@ const customStyles = {
 
 const ProjectModal = props => {
   return (
-    <Modal
-      onRequestClose={props.closeModal}
-      style={customStyles}
-      isOpen={props.show}
-      closeTimeoutMS={100}
-    >
-      <div className={Classes.modalContainer}>
-        <div className={Classes.projectImage}>
-          <Slider>
-            {props.post.node.frontmatter.galleryImages.map((image, index) => (
-              <div className={Classes.imageContainer} key={`image-${index}`}>
-                <Img fluid={image} className={Classes.image} />
+    <Suspense fallback={null}>
+      <AsyncModal
+        onRequestClose={props.closeModal}
+        style={customStyles}
+        isOpen={props.show}
+        closeTimeoutMS={100}
+      >
+        <div className={Classes.modalContainer}>
+          <div className={Classes.projectImage}>
+            <Slider>
+              {props.post.node.frontmatter.galleryImages.map((image, index) => (
+                <div className={Classes.imageContainer} key={`image-${index}`}>
+                  <Img fluid={image} className={Classes.image} />
+                </div>
+              ))}
+            </Slider>
+          </div>
+          <div className={Classes.projectInfo}>
+            <IoIosClose
+              size="2.5em"
+              style={{
+                position: "absolute",
+                top: 0,
+                right: 0,
+                margin: "1rem",
+                cursor: "pointer",
+              }}
+              onClick={props.closeModal}
+            />
+            <div className={Classes.projectInfo__header}>
+              <h2 className={Classes.projectInfo__title}>
+                {props.post.node.frontmatter.title}
+              </h2>
+              <div className={Classes.projectInfo__tech}>
+                {props.post.node.frontmatter.technologies &&
+                  props.post.node.frontmatter.technologies.map(
+                    (techItem, i) => (
+                      <span
+                        key={`tech-${i}`}
+                        className={Classes.projectInfo__techItem}
+                      >
+                        {techItem}
+                      </span>
+                    )
+                  )}
               </div>
-            ))}
-          </Slider>
-        </div>
-        <div className={Classes.projectInfo}>
-          <IoIosClose
-            size="2.5em"
-            style={{
-              position: "absolute",
-              top: 0,
-              right: 0,
-              margin: "1rem",
-              cursor: "pointer",
-            }}
-            onClick={props.closeModal}
-          />
-          <div className={Classes.projectInfo__header}>
-            <h2 className={Classes.projectInfo__title}>
-              {props.post.node.frontmatter.title}
-            </h2>
-            <div className={Classes.projectInfo__tech}>
-              {props.post.node.frontmatter.technologies &&
-                props.post.node.frontmatter.technologies.map((techItem, i) => (
-                  <span
-                    key={`tech-${i}`}
-                    className={Classes.projectInfo__techItem}
-                  >
-                    {techItem}
-                  </span>
-                ))}
             </div>
-          </div>
-          <div className={Classes.projectInfo__body}>
-            <h4 className={Classes.projectInfo__title}>ABOUT</h4>
-            <p className={Classes.projectInfo__description}>
-              {props.post.node.frontmatter.description}
-            </p>
-          </div>
-          <div className={Classes.projectInfo__footer}>
-            <div className={Classes.projectLinks}>
-              <a
-                className={`button ${Classes.projectLink}`}
-                href={props.post.node.frontmatter.url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <IoIosLink className={Classes.controlIcon} size="1.25em" />
-                URL
-              </a>
-              <a
-                className={`button ${Classes.projectLink}`}
-                href={props.post.node.frontmatter.github}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <IoLogoGithub className={Classes.controlIcon} size="1.25em" />
-                Github
-              </a>
+            <div className={Classes.projectInfo__body}>
+              <h4 className={Classes.projectInfo__title}>ABOUT</h4>
+              <p className={Classes.projectInfo__description}>
+                {props.post.node.frontmatter.description}
+              </p>
+            </div>
+            <div className={Classes.projectInfo__footer}>
+              <div className={Classes.projectLinks}>
+                <a
+                  className={`button ${Classes.projectLink}`}
+                  href={props.post.node.frontmatter.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <IoIosLink className={Classes.controlIcon} size="1.25em" />
+                  URL
+                </a>
+                <a
+                  className={`button ${Classes.projectLink}`}
+                  href={props.post.node.frontmatter.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <IoLogoGithub className={Classes.controlIcon} size="1.25em" />
+                  Github
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Modal>
+      </AsyncModal>
+    </Suspense>
   )
 }
 
