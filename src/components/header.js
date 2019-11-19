e59b4ed5d7020f4c21b0147756f82303f51296df
1qk1 <1qk231@gmail.com>
2019-11-19 19:45:55 +0200

    remove lazy loading in particles

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,8 @@
 import React, { PureComponent } from "react"
 import "./header.module.scss"
-// import Particles from "react-particles-js"
+import Particles from "react-particles-js"
 import particlesConfig from "../shared/particlesConfig"
 import Nav from "./nav"
-import { loadable } from "@loadable/component"
-
-const AsyncParticles = loadable(() => import("react-particles-js"))
 
 class Header extends PureComponent {
   render() {
@@ -13,7 +10,7 @@ class Header extends PureComponent {
       <>
         <Nav />
         <header>
-          <AsyncParticles params={particlesConfig} />
+          <Particles params={particlesConfig} />
         </header>
       </>
     )
