984b9d72efc4e2d45cb9f4c73f4500970c31db74
Panagiotis <1qk231@gmail.com>
2019-11-20 21:09:56 +0200

    swap background and theme color in manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,8 +52,8 @@ module.exports = {
         name: `Panagiotis Skarlas Portfolio`,
         short_name: `Panagiotis Skarlas`,
         start_url: `/`,
-        background_color: `#333`,
-        theme_color: `#efebd8`,
+        background_color: `#efebd8`,
+        theme_color: `#333`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
