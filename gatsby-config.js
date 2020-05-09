0f79e55b2ff1e2567bc38522bdac6dd548ce3959
Panagiotis <1qk231@gmail.com>
2020-05-09 17:39:47 +0300

    replace amplitude analytics with matomo

--- gatsby-config.js
+++ gatsby-config.js
@@ -96,27 +96,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-amplitude-analytics`,
+      resolve: "gatsby-plugin-matomo",
       options: {
-        // Specify the API key for your Amplitude Project (required)
-        apiKey: process.env.AMPLITUDE_API_KEY,
-        // Puts tracking script in the head instead of the body (optional)
-        head: false,
-        // Prevents loading Amplitude and logging events if visitors have "Do Not Track" enabled (optional)
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths (optional)
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Override the default event types (optional)
-        eventTypes: {
-          outboundLinkClick: "OUTBOUND_LINK_CLICK",
-          pageView: "PAGE_VIEW",
-        },
-        // Amplitude JS SDK configuration options (optional)
-        amplitudeConfig: {
-          saveEvents: true,
-          includeUtm: true,
-          includeReferrer: true,
-        },
+        siteId: "1",
+        matomoUrl: "//analytics.skarlas.cc",
+        siteUrl: "https://skarlas.cc",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
